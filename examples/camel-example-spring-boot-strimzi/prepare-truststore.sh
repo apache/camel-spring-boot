c24b5dbc7b29dd1c029f7fa0440c9aa882d81714
Aykut Bulgu <mabulgu@gmail.com>
2020-03-11 13:59:47 +0300

    change readme and scripts & fix pom

--- examples/camel-example-spring-boot-strimzi/prepare-truststore.sh
+++ examples/camel-example-spring-boot-strimzi/prepare-truststore.sh
@@ -2,10 +2,10 @@
 
 rm truststore.jks
 
-kubectl extract secret/my-cluster-cluster-ca-cert --keys=ca.crt --to=- -n camel-example-strimzi > ca.crt
+kubectl get secret/my-cluster-cluster-ca-cert -o 'go-template={{index .data "ca.crt"}}' -n camel-example-strimzi | base64 -D > ca.crt
 
 echo "yes" | keytool -import -trustcacerts -file ca.crt -keystore truststore.jks -storepass 123456
 
-kubectl create configmap truststore-config --from-file=truststore.jks=truststore.jks -n camel-example-strimzi
+kubectl delete configmap truststore-config && kubectl create configmap truststore-config --from-file=truststore.jks=truststore.jks -n camel-example-strimzi
 
 rm ca.crt
