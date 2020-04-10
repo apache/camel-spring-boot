a9184dde6b1bd5c022141059fda841fb610a90a8
Andrea Cosentino <ancosen@gmail.com>
2020-04-10 14:59:31 +0200

    Regen after AWS2-EKS update

--- components-starter/camel-aws2-eks-starter/src/main/java/org/apache/camel/component/aws2/eks/springboot/EKS2ComponentConfiguration.java
+++ components-starter/camel-aws2-eks-starter/src/main/java/org/apache/camel/component/aws2/eks/springboot/EKS2ComponentConfiguration.java
@@ -65,6 +65,10 @@ public class EKS2ComponentConfiguration
      */
     private EKS2Operations operation;
     /**
+     * If we want to use a POJO request as body or not
+     */
+    private Boolean pojoRequest = false;
+    /**
      * To define a proxy host when instantiating the EKS client
      */
     private String proxyHost;
@@ -126,6 +130,14 @@ public class EKS2ComponentConfiguration
 
     public void setOperation(EKS2Operations operation) {
         this.operation = operation;
+    }
+
+    public Boolean getPojoRequest() {
+        return pojoRequest;
+    }
+
+    public void setPojoRequest(Boolean pojoRequest) {
+        this.pojoRequest = pojoRequest;
     }
 
     public String getProxyHost() {
