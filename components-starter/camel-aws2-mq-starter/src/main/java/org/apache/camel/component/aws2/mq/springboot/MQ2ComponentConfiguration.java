2540523c6811f91c614029d53ca0e9c6ed330764
Andrea Cosentino <ancosen@gmail.com>
2020-04-15 12:54:34 +0200

    Regen after aws2-mq updates

--- components-starter/camel-aws2-mq-starter/src/main/java/org/apache/camel/component/aws2/mq/springboot/MQ2ComponentConfiguration.java
+++ components-starter/camel-aws2-mq-starter/src/main/java/org/apache/camel/component/aws2/mq/springboot/MQ2ComponentConfiguration.java
@@ -65,6 +65,10 @@ public class MQ2ComponentConfiguration
      */
     private MQ2Operations operation;
     /**
+     * If we want to use a POJO request as body or not
+     */
+    private Boolean pojoRequest = false;
+    /**
      * To define a proxy host when instantiating the MQ client
      */
     private String proxyHost;
@@ -126,6 +130,14 @@ public class MQ2ComponentConfiguration
 
     public void setOperation(MQ2Operations operation) {
         this.operation = operation;
+    }
+
+    public Boolean getPojoRequest() {
+        return pojoRequest;
+    }
+
+    public void setPojoRequest(Boolean pojoRequest) {
+        this.pojoRequest = pojoRequest;
     }
 
     public String getProxyHost() {
