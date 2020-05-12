5507b0a70c6a0157c75240a5b787e1c705941c20
Andrea Cosentino <ancosen@gmail.com>
2020-05-12 17:08:27 +0200

    Regen

--- components-starter/camel-robotframework-starter/src/main/java/org/apache/camel/component/robotframework/springboot/RobotFrameworkComponentConfiguration.java
+++ components-starter/camel-robotframework-starter/src/main/java/org/apache/camel/component/robotframework/springboot/RobotFrameworkComponentConfiguration.java
@@ -38,6 +38,14 @@ public class RobotFrameworkComponentConfiguration
      */
     private Boolean enabled;
     /**
+     * Sets whether the context map should allow access to all details. By
+     * default only the message body and headers can be accessed. This option
+     * can be enabled for full access to the current Exchange and CamelContext.
+     * Doing so impose a potential security risk as this opens access to the
+     * full power of CamelContext API.
+     */
+    private Boolean allowContextMapAll = false;
+    /**
      * Whether to allow to use resource template from header or not (default
      * false). Enabling this allows to specify dynamic templates via message
      * header. However this can be seen as a potential security vulnerability if
@@ -274,6 +282,14 @@ public class RobotFrameworkComponentConfiguration
      * org.apache.camel.component.robotframework.RobotFrameworkCamelConfiguration type.
      */
     private String configuration;
+
+    public Boolean getAllowContextMapAll() {
+        return allowContextMapAll;
+    }
+
+    public void setAllowContextMapAll(Boolean allowContextMapAll) {
+        this.allowContextMapAll = allowContextMapAll;
+    }
 
     public Boolean getAllowTemplateFromHeader() {
         return allowTemplateFromHeader;
