627360ee79c1f8650112e331dc9d30fa029fb691
amdmdi <42283870+amdmdi@users.noreply.github.com>
2020-07-04 14:58:52 +0200

    CAMEL-14565: Add description to spring properties
    Copied properties from parent classes to include javadoc because
    spring-boot-configuration-processor detects inherited properties by their public
    getters/setters but can not find the corresponding javadoc used for descriptions in
    generated adoc

--- components-starter/camel-consul-starter/src/main/java/org/apache/camel/component/consul/springboot/cloud/ConsulServiceRegistryConfiguration.java
+++ components-starter/camel-consul-starter/src/main/java/org/apache/camel/component/consul/springboot/cloud/ConsulServiceRegistryConfiguration.java
@@ -16,8 +16,15 @@
  */
 package org.apache.camel.component.consul.springboot.cloud;
 
+import java.math.BigInteger;
+import java.time.Duration;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import com.orbitz.consul.option.ConsistencyMode;
+
+import org.apache.camel.support.jsse.SSLContextParameters;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "camel.component.consul.service-registry")
@@ -73,4 +80,179 @@ public class ConsulServiceRegistryConfiguration extends org.apache.camel.compone
     public void setOrder(Integer order) {
         this.order = order;
     }
+
+
+
+    //
+    // Fields copied from org.apache.camel.component.consul.cloud.ConsulServiceRegistryConfiguration
+    // to add javadoc which is used by spring-boot-configuration-processor
+    // to generate descritpions for inherited properties
+    // ----------------
+
+    /**
+     * Should we remove all the registered services know by this registry on
+     * stop?
+     */
+    private boolean deregisterServicesOnStop = true;
+
+    /**
+     * Should we override the service host if given ?
+     */
+    private boolean overrideServiceHost = true;
+
+    /**
+     * Service host.
+     */
+    private String serviceHost;
+
+    /**
+     * The time (in seconds) to live for TTL checks. Default is 1 minute.
+     */
+    private int checkTtl = 60;
+
+    /**
+     * How often (in seconds) a service has to be marked as healthy if its check
+     * is TTL or how often the check should run. Default is 5 seconds.
+     */
+    private int checkInterval = 5;
+
+    /**
+     * How long (in seconds) to wait to deregister a service in case of unclean
+     * shutdown. Default is 1 hour.
+     */
+    private int deregisterAfter = 3600;
+
+    //
+    // Fields copied from org.apache.camel.component.consul.cluster.ConsulClusterConfiguration
+    // ------------------------------------------
+    /**
+     * 
+     */
+    private int sessionTtl = 60;
+
+    /**
+     * 
+     */
+    private int sessionLockDelay = 5;
+
+    /**
+     * 
+     */
+    private int sessionRefreshInterval = 5;
+
+    /**
+     * 
+     */
+    private String rootPath = "/camel";
+
+    // 
+    // Fields copied from org.apache.camel.component.consul.ConsulClientConfiguration
+    // ---------------
+
+    /**
+     * The Consul agent URL
+     */
+    private String url;
+
+    /**
+     * Use datacenter instead
+     */
+    private String dc;
+
+    /**
+     * The data center
+     */
+    private String datacenter;
+
+    /**
+     * The near node to use for queries.
+     */
+    private String nearNode;
+
+    /**
+     * The note meta-data to use for queries.
+     */
+    private List<String> nodeMeta;
+
+    /**
+     * The consistencyMode used for queries, default ConsistencyMode.DEFAULT
+     */
+    private ConsistencyMode consistencyMode = ConsistencyMode.DEFAULT;
+
+    /**
+     * Set tags. You can separate multiple tags by comma.
+     */
+    private Set<String> tags;
+
+    /**
+     * SSL configuration using an
+     * org.apache.camel.support.jsse.SSLContextParameters instance.
+     */
+    private SSLContextParameters sslContextParameters;
+
+    /**
+     * Sets the ACL token to be used with Consul
+     */
+    private String aclToken;
+
+    /**
+     * Sets the username to be used for basic authentication
+     */
+    private String userName;
+
+    /**
+     * Sets the password to be used for basic authentication
+     */
+    private String password;
+
+    /**
+     * Use connectTimeout instead
+     */
+    private Long connectTimeoutMillis;
+
+    /**
+     * Connect timeout for OkHttpClient
+     */
+    private Duration connectTimeout;
+
+    /**
+     * Use readTimeout instead.
+     */
+    private Long readTimeoutMillis;
+
+    /**
+     * Read timeout for OkHttpClient
+     */
+    private Duration readTimeout;
+
+    /**
+     * Use writeTimeout instead.
+     */
+    private Long writeTimeoutMillis;
+
+    /**
+     * Write timeout for OkHttpClient
+     */
+    private Duration writeTimeout;
+
+    /**
+     * Configure if the AgentClient should attempt a ping before returning the
+     * Consul instance
+     */
+    private boolean pingInstance = true;
+
+    /**
+     * The time (in seconds) to wait for a watch event, default 10 seconds
+     */
+    private Integer blockSeconds = 10;
+
+    /**
+     * The first index for watch for, default 0
+     */
+    private BigInteger firstIndex = BigInteger.valueOf(0L);
+
+    /**
+     * Recursively watch, default false
+     */
+    private boolean recursive;
 }
