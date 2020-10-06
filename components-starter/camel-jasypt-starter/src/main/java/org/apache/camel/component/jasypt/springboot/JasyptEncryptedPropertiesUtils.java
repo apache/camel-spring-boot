b62297e6257b1438c333c1a99c52625727cf6063
Luigi De Masi <ldemasi@redhat.com>
2020-10-06 18:10:49 +0200

    Fix CamelJasyptTest integration test

--- components-starter/camel-jasypt-starter/src/main/java/org/apache/camel/component/jasypt/springboot/JasyptEncryptedPropertiesUtils.java
+++ components-starter/camel-jasypt-starter/src/main/java/org/apache/camel/component/jasypt/springboot/JasyptEncryptedPropertiesUtils.java
@@ -16,12 +16,28 @@
  */
 package org.apache.camel.component.jasypt.springboot;
 
+import org.jasypt.encryption.pbe.config.EnvironmentStringPBEConfig;
+import org.jasypt.exceptions.EncryptionInitializationException;
+import org.jasypt.iv.IvGenerator;
+import org.jasypt.iv.NoIvGenerator;
+import org.jasypt.iv.RandomIvGenerator;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.apache.camel.util.ObjectHelper.isNotEmpty;
+import static org.apache.camel.util.StringHelper.after;
+
 public class JasyptEncryptedPropertiesUtils {
 
+    private static final String SYSTEM_ENVIRONMENT_PREFIX = "sysenv:";
+
+    private static final String SYSTEM_PROPERTIES_PREFIX = "sys:";
+
+    /**
+     *  Algorithms that mandatory require initialization vector
+     */
     static final Set<String> ALGORITHMS_THAT_REQUIRE_IV = new HashSet<>(
             Arrays.asList(
                     "PBEWITHHMACSHA1ANDAES_128",
@@ -37,7 +53,80 @@ public class JasyptEncryptedPropertiesUtils {
             )
     );
 
+    /**
+     * test if algorithm requires an initialization vector
+     * @param algorithm the algorithm to test
+     * @return true if the algorithm requires initialization vector, false otherwise
+     */
     static boolean isIVNeeded(String algorithm) {
-        return ALGORITHMS_THAT_REQUIRE_IV.contains(algorithm.toUpperCase());
+        if (isNotBlank(algorithm)) {
+            return ALGORITHMS_THAT_REQUIRE_IV.contains(algorithm.toUpperCase());
+        }
+        return false;
+    }
+
+    /**
+     * Checks if a CharSequence is empty (""), null or whitespace only.
+     * @param cs  the CharSequence to check, may be null
+     * @return true if the CharSequence is null, empty or whitespace only
+     */
+    static boolean isBlank(final CharSequence cs) {
+        final int strLen = cs == null ? 0 : cs.length();
+        if (strLen == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(cs.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks if a CharSequence is not empty (""), not null or not whitespace only.
+     * @param cs the CharSequence to check, may be null
+     * @return true if the CharSequence is not null, not empty or not whitespace only
+     */
+    static boolean isNotBlank(final CharSequence cs) {
+        return !isBlank(cs);
+    }
+
+
+    static void parsePassword(EnvironmentStringPBEConfig environmentStringPBEConfig, JasyptEncryptedPropertiesConfiguration configuration) {
+        String passwordReference = configuration.getPassword();
+        if (isNotEmpty(passwordReference) && passwordReference.startsWith(SYSTEM_ENVIRONMENT_PREFIX)) {
+            environmentStringPBEConfig.setPasswordEnvName(after(passwordReference, SYSTEM_ENVIRONMENT_PREFIX));
+            return;
+        }
+        if (isNotEmpty(passwordReference) && passwordReference.startsWith(SYSTEM_PROPERTIES_PREFIX)) {
+            environmentStringPBEConfig.setPasswordSysPropertyName(after(passwordReference, SYSTEM_PROPERTIES_PREFIX));
+            return;
+        }
+        environmentStringPBEConfig.setPassword(passwordReference);
+    }
+
+    static IvGenerator getIVGenerator(JasyptEncryptedPropertiesConfiguration configuration) {
+        String ivGeneratorClassName = configuration.getIvGeneratorClassName();
+        String algorithm = configuration.getAlgorithm();
+        if (isBlank(ivGeneratorClassName)) {
+            return isIVNeeded(algorithm) ? new RandomIvGenerator() : new NoIvGenerator();
+        }
+        IvGenerator ivGenerator = loadClass(ivGeneratorClassName);
+        return ivGenerator;
+    }
+
+    /**
+     * search and load the class identified by className parameter
+     * @param className fully qualified class name to be loaded
+     * @return a new instance of type className
+     */
+    static <T> T loadClass(String className) {
+        try {
+            final Class clazz = Thread.currentThread().getContextClassLoader().loadClass(className);
+            return (T) clazz.newInstance();
+        } catch (Exception e) {
+            throw new EncryptionInitializationException(e);
+        }
     }
 }
---
+++ components-starter/camel-jasypt-starter/src/test/java/org/apache/camel/component/jasypt/springboot/AbstractEncryptedPropertiesIvGeneratorAutoDetectionTest.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.jasypt.springboot;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.EnvironmentStringPBEConfig;
+import org.jasypt.iv.NoIvGenerator;
+import org.jasypt.iv.RandomIvGenerator;
+import org.jasypt.salt.RandomSaltGenerator;
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.apache.camel.component.jasypt.springboot.JasyptEncryptedPropertiesUtils.isIVNeeded;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public abstract class AbstractEncryptedPropertiesIvGeneratorAutoDetectionTest {
+
+    protected  final Logger LOG = LoggerFactory.getLogger(this.getClass());
+
+    protected static final String SUN_JCE_PROVIDER_NAME = "SunJCE";
+    protected static final String BOUNCY_CASTLE_PROVIDER_NAME = "BC";
+
+
+    String stringToEncrypt = "A password-cracker walks into a bar. Orders a beer. Then a Beer. Then a BEER. beer. b33r. BeeR. Be3r. bEeR. bE3R. BeEr";
+    String password = "s0m3R@nD0mP@ssW0rD";
+
+
+    @Parameter
+    public String algorithm;
+
+    protected String provider;
+
+    @Test
+    public void testEncryptionAndDecryption() {
+
+        LOG.info("Testing Algorithm: '{}', requires IV: {}", algorithm, isIVNeeded(algorithm));
+
+        // Create a ByteArrayOutputStream so that we can get the output
+        // from the call to print
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        // Change System.out to point out to our stream
+        System.setOut(new PrintStream(baos));
+
+        EnvironmentStringPBEConfig environmentStringPBEConfig = new EnvironmentStringPBEConfig();
+        environmentStringPBEConfig.setAlgorithm(algorithm);
+        environmentStringPBEConfig.setIvGenerator(isIVNeeded(algorithm)?new RandomIvGenerator():new NoIvGenerator());
+        environmentStringPBEConfig.setSaltGenerator(new RandomSaltGenerator());
+        environmentStringPBEConfig.setProviderName(provider);
+        environmentStringPBEConfig.setPassword(password);
+
+        StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+        standardPBEStringEncryptor.setConfig(environmentStringPBEConfig);
+
+        // Testing Encryption.
+        String encrypted = standardPBEStringEncryptor.encrypt(stringToEncrypt);
+
+        // Testing Decryption:
+        String actualDecriptedString = standardPBEStringEncryptor.decrypt(encrypted);
+
+        //Assertions
+        assertThat(actualDecriptedString).isEqualTo(stringToEncrypt);
+    }
+
+}
