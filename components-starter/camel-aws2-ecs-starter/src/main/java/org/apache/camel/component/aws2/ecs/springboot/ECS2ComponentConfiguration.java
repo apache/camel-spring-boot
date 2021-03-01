abebdc1588d91b1761ceeed97de1fb198c95c6ca
Andrea Cosentino <ancosen@gmail.com>
2021-03-01 08:39:24 +0100

    CAMEL-16171 - Add uri-endpoint-override options to all AWS2 components - AWS2-ECS component

--- components-starter/camel-aws2-ecs-starter/src/main/java/org/apache/camel/component/aws2/ecs/springboot/ECS2ComponentConfiguration.java
+++ components-starter/camel-aws2-ecs-starter/src/main/java/org/apache/camel/component/aws2/ecs/springboot/ECS2ComponentConfiguration.java
@@ -67,6 +67,11 @@ public class ECS2ComponentConfiguration
      */
     private ECS2Operations operation;
     /**
+     * Set the need for overidding the endpoint. This option needs to be used in
+     * combination with uriEndpointOverride option
+     */
+    private Boolean overrideEndpoint = false;
+    /**
      * If we want to use a POJO request as body or not
      */
     private Boolean pojoRequest = false;
@@ -92,6 +97,11 @@ public class ECS2ComponentConfiguration
      * If we want to trust all certificates in case of overriding the endpoint
      */
     private Boolean trustAllCertificates = false;
+    /**
+     * Set the overriding uri endpoint. This option needs to be used in
+     * combination with overrideEndpoint option
+     */
+    private String uriEndpointOverride;
     /**
      * Whether autowiring is enabled. This is used for automatic autowiring
      * options (the option must be marked as autowired) by looking up in the
@@ -142,6 +152,14 @@ public class ECS2ComponentConfiguration
         this.operation = operation;
     }
 
+    public Boolean getOverrideEndpoint() {
+        return overrideEndpoint;
+    }
+
+    public void setOverrideEndpoint(Boolean overrideEndpoint) {
+        this.overrideEndpoint = overrideEndpoint;
+    }
+
     public Boolean getPojoRequest() {
         return pojoRequest;
     }
@@ -188,6 +206,14 @@ public class ECS2ComponentConfiguration
 
     public void setTrustAllCertificates(Boolean trustAllCertificates) {
         this.trustAllCertificates = trustAllCertificates;
+    }
+
+    public String getUriEndpointOverride() {
+        return uriEndpointOverride;
+    }
+
+    public void setUriEndpointOverride(String uriEndpointOverride) {
+        this.uriEndpointOverride = uriEndpointOverride;
     }
 
     public Boolean getAutowiredEnabled() {
