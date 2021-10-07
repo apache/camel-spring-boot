37642fe58a5a6024cbd21c342fc1ec92b4a0755d
David Jencks <djencks@apache.org>
2021-10-06 23:06:45 -0700

    Tooling and template changes to generate autoconfig option tables and lists of starters
    (docs/pom.xml) set up copying autoconfigure metadata json files to examples/json
    
    (UpdateDocComponentsListMojo) correct check for local component
    
    (UpdateSpringBootAutoConfigurationReadmeMojo) copy generated auto-config json file to src/main/docs
    
    add starter.adoc template
    
    Set index table format as attribute in component descriptor
    
    list.adoc with generated tables
    
    accomodate explicit starter-artifactid in list tables and starter partial

--- tooling/camel-spring-boot-generator-maven-plugin/src/main/java/org/apache/camel/springboot/maven/UpdateSpringBootAutoConfigurationReadmeMojo.java
+++ tooling/camel-spring-boot-generator-maven-plugin/src/main/java/org/apache/camel/springboot/maven/UpdateSpringBootAutoConfigurationReadmeMojo.java
@@ -19,7 +19,10 @@ package org.apache.camel.springboot.maven;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -53,6 +56,10 @@ import static org.apache.camel.tooling.util.PackageHelper.writeText;
 @Mojo(name = "update-spring-boot-auto-configuration-readme", threadSafe = true)
 public class UpdateSpringBootAutoConfigurationReadmeMojo extends AbstractMojo {
 
+    private static final String NO_OPTIONS = "{\n" +
+            "  \"properties\": []\n" +
+            "}";
+
     /**
      * The maven project.
      */
@@ -65,18 +72,6 @@ public class UpdateSpringBootAutoConfigurationReadmeMojo extends AbstractMojo {
      */
     @Parameter(defaultValue = "${project.build.directory}")
     protected File buildDir;
-
-    /**
-     * Whether to fail the build fast if any Warnings was detected.
-     */
-    @Parameter
-    protected Boolean failFast;
-
-    /**
-     * Whether to fail if an option has no documentation.
-     */
-    @Parameter
-    protected Boolean failOnMissingDescription;
 
     /**
      * build context to check changed files and mark them for refresh (used for
@@ -94,57 +89,39 @@ public class UpdateSpringBootAutoConfigurationReadmeMojo extends AbstractMojo {
         }
     }
 
+    // Copies json file to src/main/docs or, if there is none, writes a no-options file to src/main/docs
+    // Is there a better way and place to do this?
     private void executeStarter(File starter) throws Exception {
+        String name = starter.getName();
+
+        //There's almost certainly a better test!
+        if ("components-starter".equals(name) || "core".equals(name)) {
+            return;
+        }
+
+        // remove camel- prefix and -starter suffix
+        if (name.startsWith("camel-")) {
+            name = name.substring(6);
+        }
+        if (name.endsWith("-starter")) {
+            name = name.substring(0, name.length() - 8);
+        }
+        String componentName = name;
+        getLog().debug("Camel component: " + componentName);
+        Path starterPath = starter.toPath();
+        Path docFolder = starterPath.resolve("src/main/docs/");
+        Files.createDirectories(docFolder);
+        Path jsonFileCopy = docFolder.resolve(componentName + ".json");
+
         File jsonFile = new File(buildDir, "classes/META-INF/spring-configuration-metadata.json");
 
-        // only if there is components we should update the documentation files
         if (jsonFile.exists()) {
-            getLog().debug("Processing Spring Boot auto-configuration file: " + jsonFile);
-            Object js = Jsoner.deserialize(new FileReader(jsonFile));
-            if (js != null) {
-                String name = starter.getName();
-
-                if (!isValidStarter(name)) {
-                    return;
-                }
-
-                if (name.startsWith("camel-")) {
-                    // skip camel-  and -starter in the end
-                    name = name.substring(6);
-                }
-                boolean isStarter = false;
-                if (name.endsWith("-starter")) {
-                    // skip camel-  and -starter in the end
-                    name = name.substring(0, name.length() - 8);
-                    isStarter = true;
-                }
-                String componentName = name;
-                getLog().debug("Camel component: " + componentName);
-                File docFolder = new File(starter,"src/main/docs/");
-                File docFile = new File(docFolder, isStarter ? componentName + "-starter.adoc" : componentName + ".adoc");
-
-                List<SpringBootAutoConfigureOptionModel> models = parseSpringBootAutoConfigureModels(jsonFile, null);
-
-                // check for missing description on options
-                boolean noDescription = false;
-                for (SpringBootAutoConfigureOptionModel o : models) {
-                    if (Strings.isEmpty(o.getDescription())) {
-                        noDescription = true;
-                        getLog().warn("Option " + o.getName() + " has no description");
-                    }
-                }
-                if (noDescription && isFailOnNoDescription()) {
-                    throw new MojoExecutionException("Failed build due failOnMissingDescription=true");
-                }
-
-                String changed = templateAutoConfigurationOptions(models, componentName);
-                boolean updated = updateAutoConfigureOptions(docFile, changed);
-                if (updated) {
-                    getLog().info("Updated doc file: " + docFile);
-                } else {
-                    getLog().debug("No changes to doc file: " + docFile);
-                }
-            }
+            getLog().debug("Copying generated Spring Boot auto-configuration file to: " + jsonFileCopy);
+            byte[] contents = Files.readAllBytes(jsonFile.toPath());
+            Files.write(jsonFileCopy, contents);
+        } else {
+            getLog().debug("Generating no-options Spring Boot auto-configuration file: " + jsonFileCopy);
+            Files.write(jsonFileCopy, NO_OPTIONS.getBytes(StandardCharsets.UTF_8));
         }
     }
 
@@ -178,120 +155,6 @@ public class UpdateSpringBootAutoConfigurationReadmeMojo extends AbstractMojo {
             return "tidyMarkup";
         }
         return componentName;
-    }
-
-    private static boolean isValidStarter(String name) {
-        return true;
-    }
-
-    private List<SpringBootAutoConfigureOptionModel> parseSpringBootAutoConfigureModels(File file, String include) throws IOException, DeserializationException {
-        getLog().debug("Parsing Spring Boot AutoConfigureModel using include: " + include);
-        List<SpringBootAutoConfigureOptionModel> answer = new ArrayList<>();
-
-        JsonObject obj = (JsonObject) Jsoner.deserialize(new FileReader(file));
-
-        JsonArray arr = obj.getCollection("properties");
-        if (arr != null && !arr.isEmpty()) {
-            arr.forEach(e -> {
-                JsonObject row = (JsonObject) e;
-                String name = row.getString("name");
-                String javaType = row.getString("type");
-                String desc = row.getStringOrDefault("description", "");
-                String defaultValue = row.getStringOrDefault("defaultValue", "");
-
-                // is the option deprecated then include that as well in the description
-                String deprecated = row.getStringOrDefault("deprecated", "");
-                String deprecationNote = row.getStringOrDefault("deprecationNote", "");
-                if ("true".equals(deprecated)) {
-                    desc = "*Deprecated* " + desc;
-                    if (!Strings.isEmpty(deprecationNote)) {
-                        if (!desc.endsWith(".")) {
-                            desc = desc + ". Deprecation note: " + deprecationNote;
-                        } else {
-                            desc = desc + " Deprecation note: " + deprecationNote;
-                        }
-                    }
-                }
-
-                // skip this special option and also if not matching the filter
-                boolean skip = name.endsWith("customizer.enabled") || include != null && !name.contains("." + include + ".");
-                if (!skip) {
-                    SpringBootAutoConfigureOptionModel model = new SpringBootAutoConfigureOptionModel();
-                    model.setName(name);
-                    model.setJavaType(javaType);
-                    model.setDefaultValue(defaultValue);
-                    model.setDescription(desc);
-                    answer.add(model);
-                }
-            });
-        }
-
-        return answer;
-    }
-
-    private boolean updateAutoConfigureOptions(File file, String changed) throws MojoExecutionException {
-        try {
-            if (!file.exists()) {
-                // include markers for new files
-                changed = "// spring-boot-auto-configure options: START\n" + changed + "\n// spring-boot-auto-configure options: END\n";
-                writeText(file, changed);
-                return true;
-            }
-
-            String text = loadText(new FileInputStream(file));
-
-            String existing = Strings.between(text, "// spring-boot-auto-configure options: START", "// spring-boot-auto-configure options: END");
-            if (existing != null) {
-                // remove leading line breaks etc
-                existing = existing.trim();
-                changed = changed.trim();
-                if (existing.equals(changed)) {
-                    return false;
-                } else {
-                    String before = Strings.before(text, "// spring-boot-auto-configure options: START");
-                    String after = Strings.after(text, "// spring-boot-auto-configure options: END");
-                    text = before + "// spring-boot-auto-configure options: START\n" + changed + "\n// spring-boot-auto-configure options: END" + after;
-                    writeText(file, text);
-                    return true;
-                }
-            } else {
-                getLog().warn("Cannot find markers in file " + file);
-                getLog().warn("Add the following markers");
-                getLog().warn("\t// spring-boot-auto-configure options: START");
-                getLog().warn("\t// spring-boot-auto-configure options: END");
-                if (isFailFast()) {
-                    throw new MojoExecutionException("Failed build due failFast=true");
-                }
-                return false;
-            }
-        } catch (Exception e) {
-            throw new MojoExecutionException("Error reading file " + file + " Reason: " + e, e);
-        }
-    }
-
-    private String templateAutoConfigurationOptions(List<SpringBootAutoConfigureOptionModel> options, String componentName) throws MojoExecutionException {
-        SpringBootModel model = new SpringBootModel();
-        model.setGroupId(project.getGroupId());
-        model.setArtifactId("camel-" + componentName + "-starter");
-        model.setVersion(project.getVersion());
-        model.setOptions(options);
-        model.setTitle(componentName);
-
-        try {
-            String template = loadText(UpdateSpringBootAutoConfigurationReadmeMojo.class.getClassLoader().getResourceAsStream("spring-boot-auto-configure-options.mvel"));
-            String out = (String) TemplateRuntime.eval(template, model, Collections.singletonMap("util", MvelHelper.INSTANCE));
-            return out;
-        } catch (Exception e) {
-            throw new MojoExecutionException("Error processing mvel template. Reason: " + e, e);
-        }
-    }
-
-    private boolean isFailFast() {
-        return failFast != null && failFast;
-    }
-
-    private boolean isFailOnNoDescription() {
-        return failOnMissingDescription != null && failOnMissingDescription;
     }
 
 }
