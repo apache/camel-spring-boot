c7749c5ec629ae7af8a4eb0d0a3b0eeebd232599
Claus Ibsen <claus.ibsen@gmail.com>
2022-02-21 10:08:03 +0100

    CAMEL-17644 - Support ability to load properties from Vault/Secrets cloud services - aws secrets manager

---
+++ core/camel-spring-boot/src/test/java/org/apache/camel/spring/boot/vault/AwsVaultConfigurationTest.java
@@ -0,0 +1,34 @@
+package org.apache.camel.spring.boot.vault;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@DirtiesContext
+@CamelSpringBootTest
+@EnableAutoConfiguration
+@SpringBootTest(
+        classes = {
+                AwsVaultConfigurationTest.class},
+        properties = {
+                "camel.vault.aws.accessKey=myAccessKey",
+                "camel.vault.aws.secretKey=mySecretKey",
+                "camel.vault.aws.region=myRegion"}
+)
+public class AwsVaultConfigurationTest {
+
+    @Autowired
+    private CamelContext camelContext;
+
+    @Test
+    public void testAwsVault() throws Exception {
+        Assertions.assertEquals("myAccessKey", camelContext.getVaultConfiguration().aws().getAccessKey());
+        Assertions.assertEquals("mySecretKey", camelContext.getVaultConfiguration().aws().getSecretKey());
+        Assertions.assertEquals("myRegion", camelContext.getVaultConfiguration().aws().getRegion());
+    }
+}
