bad66719726cdb5b4b8d2ac322d6c08492b795a7
Claus Ibsen <claus.ibsen@gmail.com>
2023-06-13 08:47:13 +0200

    Regen

--- components-starter/camel-aws2-cw-starter/src/main/java/org/apache/camel/component/aws2/cw/springboot/Cw2ComponentConfiguration.java
+++ components-starter/camel-aws2-cw-starter/src/main/java/org/apache/camel/component/aws2/cw/springboot/Cw2ComponentConfiguration.java
@@ -70,6 +70,11 @@ public class Cw2ComponentConfiguration
      */
     private Boolean overrideEndpoint = false;
     /**
+     * If using a profile credentials provider this parameter will set the
+     * profile name
+     */
+    private String profileCredentialsName;
+    /**
      * To define a proxy host when instantiating the CW client
      */
     private String proxyHost;
@@ -110,6 +115,11 @@ public class Cw2ComponentConfiguration
      * in.
      */
     private Boolean useDefaultCredentialsProvider = false;
+    /**
+     * Set whether the Cloudwatch client should expect to load credentials
+     * through a profile credentials provider.
+     */
+    private Boolean useProfileCredentialsProvider = false;
     /**
      * The metric value
      */
@@ -170,6 +180,14 @@ public class Cw2ComponentConfiguration
 
     public void setOverrideEndpoint(Boolean overrideEndpoint) {
         this.overrideEndpoint = overrideEndpoint;
+    }
+
+    public String getProfileCredentialsName() {
+        return profileCredentialsName;
+    }
+
+    public void setProfileCredentialsName(String profileCredentialsName) {
+        this.profileCredentialsName = profileCredentialsName;
     }
 
     public String getProxyHost() {
@@ -243,6 +261,15 @@ public class Cw2ComponentConfiguration
     public void setUseDefaultCredentialsProvider(
             Boolean useDefaultCredentialsProvider) {
         this.useDefaultCredentialsProvider = useDefaultCredentialsProvider;
+    }
+
+    public Boolean getUseProfileCredentialsProvider() {
+        return useProfileCredentialsProvider;
+    }
+
+    public void setUseProfileCredentialsProvider(
+            Boolean useProfileCredentialsProvider) {
+        this.useProfileCredentialsProvider = useProfileCredentialsProvider;
     }
 
     public Double getValue() {
