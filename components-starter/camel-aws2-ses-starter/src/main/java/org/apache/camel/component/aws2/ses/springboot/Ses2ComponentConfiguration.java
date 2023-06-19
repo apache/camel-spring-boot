23f2e3825f93334385f386aaf84f36f6e7e53ed9
Claus Ibsen <claus.ibsen@gmail.com>
2023-06-19 16:23:14 +0200

    Regen

--- components-starter/camel-aws2-ses-starter/src/main/java/org/apache/camel/component/aws2/ses/springboot/Ses2ComponentConfiguration.java
+++ components-starter/camel-aws2-ses-starter/src/main/java/org/apache/camel/component/aws2/ses/springboot/Ses2ComponentConfiguration.java
@@ -80,6 +80,11 @@ public class Ses2ComponentConfiguration
      */
     private Boolean overrideEndpoint = false;
     /**
+     * If using a profile credentials provider this parameter will set the
+     * profile name
+     */
+    private String profileCredentialsName;
+    /**
      * To define a proxy host when instantiating the SES client
      */
     private String proxyHost;
@@ -132,6 +137,11 @@ public class Ses2ComponentConfiguration
      * in.
      */
     private Boolean useDefaultCredentialsProvider = false;
+    /**
+     * Set whether the SES client should expect to load credentials through a
+     * profile credentials provider.
+     */
+    private Boolean useProfileCredentialsProvider = false;
     /**
      * Whether autowiring is enabled. This is used for automatic autowiring
      * options (the option must be marked as autowired) by looking up in the
@@ -204,6 +214,14 @@ public class Ses2ComponentConfiguration
 
     public void setOverrideEndpoint(Boolean overrideEndpoint) {
         this.overrideEndpoint = overrideEndpoint;
+    }
+
+    public String getProfileCredentialsName() {
+        return profileCredentialsName;
+    }
+
+    public void setProfileCredentialsName(String profileCredentialsName) {
+        this.profileCredentialsName = profileCredentialsName;
     }
 
     public String getProxyHost() {
@@ -293,6 +311,15 @@ public class Ses2ComponentConfiguration
     public void setUseDefaultCredentialsProvider(
             Boolean useDefaultCredentialsProvider) {
         this.useDefaultCredentialsProvider = useDefaultCredentialsProvider;
+    }
+
+    public Boolean getUseProfileCredentialsProvider() {
+        return useProfileCredentialsProvider;
+    }
+
+    public void setUseProfileCredentialsProvider(
+            Boolean useProfileCredentialsProvider) {
+        this.useProfileCredentialsProvider = useProfileCredentialsProvider;
     }
 
     public Boolean getAutowiredEnabled() {
