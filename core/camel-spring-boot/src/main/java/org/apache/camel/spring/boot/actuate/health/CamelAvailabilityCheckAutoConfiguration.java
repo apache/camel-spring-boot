c0757ef71be5dfd4034f4ad5f6731bbcb1a788f8
Croway <federico.mariani.1990@gmail.com>
2023-11-08 15:59:10 +0100

    CAMEL-19648: readiness/liveness probes

---
+++ core/camel-spring-boot/src/main/java/org/apache/camel/spring/boot/actuate/health/CamelAvailabilityCheckAutoConfiguration.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.spring.boot.actuate.health;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.boot.CamelAutoConfiguration;
+import org.apache.camel.spring.boot.actuate.health.liveness.CamelLivenessStateHealthIndicator;
+import org.apache.camel.spring.boot.actuate.health.readiness.CamelReadinessStateHealthIndicator;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.availability.ApplicationAvailabilityAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.availability.ApplicationAvailability;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+@AutoConfigureAfter({CamelAutoConfiguration.class, ApplicationAvailabilityAutoConfiguration.class})
+@ConditionalOnBean(CamelAutoConfiguration.class)
+public class CamelAvailabilityCheckAutoConfiguration {
+
+	@Bean
+	@ConditionalOnMissingBean(CamelLivenessStateHealthIndicator.class)
+	public CamelLivenessStateHealthIndicator camelLivenessStateHealthIndicator(ApplicationAvailability applicationAvailability,
+																			   CamelContext camelContext) {
+		return new CamelLivenessStateHealthIndicator(applicationAvailability, camelContext);
+	}
+
+	@Bean
+	@ConditionalOnMissingBean({CamelReadinessStateHealthIndicator.class})
+	public CamelReadinessStateHealthIndicator camelReadinessStateHealthIndicator(
+			ApplicationAvailability applicationAvailability,
+			CamelContext camelContext) {
+		return new CamelReadinessStateHealthIndicator(applicationAvailability, camelContext);
+	}
+}
---
+++ core/camel-spring-boot/src/main/java/org/apache/camel/spring/boot/actuate/health/liveness/CamelLivenessStateHealthIndicator.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.spring.boot.actuate.health.liveness;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.health.HealthCheck;
+import org.apache.camel.health.HealthCheckHelper;
+import org.apache.camel.spring.boot.actuate.health.readiness.CamelReadinessStateHealthIndicator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.availability.LivenessStateHealthIndicator;
+import org.springframework.boot.availability.ApplicationAvailability;
+import org.springframework.boot.availability.AvailabilityState;
+import org.springframework.boot.availability.LivenessState;
+
+import java.util.Collection;
+
+public class CamelLivenessStateHealthIndicator extends LivenessStateHealthIndicator {
+
+	private static final Logger LOG = LoggerFactory.getLogger(CamelLivenessStateHealthIndicator.class);
+
+	private CamelContext camelContext;
+
+	public CamelLivenessStateHealthIndicator(
+			ApplicationAvailability availability,
+			CamelContext camelContext) {
+		super(availability);
+
+		this.camelContext = camelContext;
+	}
+
+	@Override
+	protected AvailabilityState getState(ApplicationAvailability applicationAvailability) {
+		Collection<HealthCheck.Result> results = HealthCheckHelper.invokeLiveness(camelContext);
+
+		boolean isLive = CamelReadinessStateHealthIndicator.checkState(results, LOG);
+
+		return isLive ?
+				LivenessState.CORRECT : LivenessState.BROKEN;
+	}
+}
