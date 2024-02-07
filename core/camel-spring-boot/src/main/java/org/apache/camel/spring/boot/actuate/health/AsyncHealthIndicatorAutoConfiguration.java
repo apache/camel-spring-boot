9c93f74c208666362e6503a9dda14dec24771d82
Claus Ibsen <claus.ibsen@gmail.com>
2024-02-07 09:38:07 +0100

    Add missing license header

--- core/camel-spring-boot/src/main/java/org/apache/camel/spring/boot/actuate/health/AsyncHealthIndicatorAutoConfiguration.java
+++ core/camel-spring-boot/src/main/java/org/apache/camel/spring/boot/actuate/health/AsyncHealthIndicatorAutoConfiguration.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.spring.boot.actuate.health;
 
 import org.apache.camel.spring.boot.actuate.health.liveness.CamelLivenessStateHealthIndicator;
@@ -24,7 +40,6 @@ import java.time.ZonedDateTime;
  * will be added in spring boot 3.2.x as a new feature in the future.
  *
  * TODO: To be refactored once async health contributors feature will be added in spring boot.
- *
  */
 @Configuration
 @ConditionalOnProperty(prefix = "camel.health", name = "async-camel-health-check", havingValue = "true")
