eb9e016846a7eae948ba56744b5b5f54cb885843
Claus Ibsen <claus.ibsen@gmail.com>
2024-02-15 13:28:33 +0100

    CAMEL-20419: Generate json metadata for @DataTypeTransformer

--- catalog/camel-catalog-provider-springboot/src/main/java/org/apache/camel/springboot/catalog/SpringBootRuntimeProvider.java
+++ catalog/camel-catalog-provider-springboot/src/main/java/org/apache/camel/springboot/catalog/SpringBootRuntimeProvider.java
@@ -34,10 +34,12 @@ public class SpringBootRuntimeProvider implements RuntimeProvider {
     private static final String COMPONENT_DIR = "org/apache/camel/springboot/catalog/components";
     private static final String DATAFORMAT_DIR = "org/apache/camel/springboot/catalog/dataformats";
     private static final String LANGUAGE_DIR = "org/apache/camel/springboot/catalog/languages";
+    private static final String TRANSFORMER_DIR = "org/apache/camel/springboot/catalog/transformers";
     private static final String OTHER_DIR = "org/apache/camel/springboot/catalog/others";
     private static final String COMPONENTS_CATALOG = "org/apache/camel/springboot/catalog/components.properties";
     private static final String DATA_FORMATS_CATALOG = "org/apache/camel/springboot/catalog/dataformats.properties";
     private static final String LANGUAGE_CATALOG = "org/apache/camel/springboot/catalog/languages.properties";
+    private static final String TRANSFORMER_CATALOG = "org/apache/camel/springboot/catalog/transformers.properties";
     private static final String OTHER_CATALOG = "org/apache/camel/springboot/catalog/others.properties";
 
     private CamelCatalog camelCatalog;
@@ -83,6 +85,11 @@ public class SpringBootRuntimeProvider implements RuntimeProvider {
     }
 
     @Override
+    public String getTransformerJSonSchemaDirectory() {
+        return TRANSFORMER_DIR;
+    }
+
+    @Override
     public String getOtherJSonSchemaDirectory() {
         return OTHER_DIR;
     }
@@ -100,6 +107,11 @@ public class SpringBootRuntimeProvider implements RuntimeProvider {
     @Override
     public List<String> findLanguageNames() {
         return findNames(LANGUAGE_CATALOG);
+    }
+
+    @Override
+    public List<String> findTransformerNames() {
+        return findNames(TRANSFORMER_CATALOG);
     }
 
     @Override
