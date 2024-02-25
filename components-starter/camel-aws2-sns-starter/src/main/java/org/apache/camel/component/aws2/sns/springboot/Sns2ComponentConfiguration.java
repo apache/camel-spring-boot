8e030828ed5348bdd45b5ef0aa0a384ebf4714fd
Claus Ibsen <claus.ibsen@gmail.com>
2024-02-25 09:24:20 +0100

    Regen

--- components-starter/camel-aws2-sns-starter/src/main/java/org/apache/camel/component/aws2/sns/springboot/Sns2ComponentConfiguration.java
+++ components-starter/camel-aws2-sns-starter/src/main/java/org/apache/camel/component/aws2/sns/springboot/Sns2ComponentConfiguration.java
@@ -74,7 +74,7 @@ public class Sns2ComponentConfiguration
      * Only for FIFO Topic. Strategy for setting the messageGroupId on the
      * message. It can be one of the following options: useConstant,
      * useExchangeId, usePropertyValue. For the usePropertyValue option, the
-     * value of property CamelAwsMessageGroupId will be used.
+     * value of property CamelAwsSnsMessageGroupId will be used.
      */
     private String messageGroupIdStrategy;
     /**
