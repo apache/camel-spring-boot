961441e53f23d93ace35b86f71ef870caeb52fcc
Claus Ibsen <claus.ibsen@gmail.com>
2024-06-14 07:44:53 +0200

    Regen

--- components-starter/camel-pubnub-starter/src/main/java/org/apache/camel/component/pubnub/springboot/PubNubComponentConfiguration.java
+++ components-starter/camel-pubnub-starter/src/main/java/org/apache/camel/component/pubnub/springboot/PubNubComponentConfiguration.java
@@ -81,12 +81,11 @@ public class PubNubComponentConfiguration
      * BLOCKS Event Handlers. These messages will go directly to any Event
      * Handlers registered on the channel. HERENOW: Obtain information about the
      * current state of a channel including a list of unique user-ids currently
-     * subscribed to the channel and the total occupancy count. WHERENOW: Obtain
-     * information about the current list of channels to which a uuid is
-     * subscribed to. GETSTATE: Used to get key/value pairs specific to a
-     * subscriber uuid. State information is supplied as a JSON object of
-     * key/value pairs SETSTATE: Used to set key/value pairs specific to a
-     * subscriber uuid GETHISTORY: Fetches historical messages of a channel.
+     * subscribed to the channel and the total occupancy count. GETSTATE: Used
+     * to get key/value pairs specific to a subscriber uuid. State information
+     * is supplied as a JSON object of key/value pairs SETSTATE: Used to set
+     * key/value pairs specific to a subscriber uuid GETHISTORY: Fetches
+     * historical messages of a channel.
      */
     private String operation;
     /**
