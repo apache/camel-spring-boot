ef514afca542480c1d8d7e97302309d10cd73353
Croway <federico.mariani.1990@gmail.com>
2026-04-16 15:08:58 +0200

    ci: Use JUnit 5 for Camel 4.14.x
    The CAMEL-23320 backport introduced test files with junit6 imports
    (camel-test-spring-junit6) which only exists in 4.19+. This fixes
    the compilation failure in camel-platform-http-starter on the
    4.14.x branch by using the correct junit5 artifact.

--- components-starter/camel-platform-http-starter/src/test/java/org/apache/camel/component/platform/http/springboot/PlatformHttpBinaryUploadTest.java
+++ components-starter/camel-platform-http-starter/src/test/java/org/apache/camel/component/platform/http/springboot/PlatformHttpBinaryUploadTest.java
@@ -19,7 +19,7 @@ package org.apache.camel.component.platform.http.springboot;
 import io.restassured.RestAssured;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.spring.boot.CamelAutoConfiguration;
-import org.apache.camel.test.spring.junit6.CamelSpringBootTest;
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
