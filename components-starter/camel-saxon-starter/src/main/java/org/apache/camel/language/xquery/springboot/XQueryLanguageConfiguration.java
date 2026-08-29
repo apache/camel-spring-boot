13fae08721a2d38bab26d80873cfd947fbb9ad7b
oscerd <5106647+oscerd@users.noreply.github.com>
2026-08-29 00:42:40 +0000

    [create-pull-request] automated change

--- components-starter/camel-saxon-starter/src/main/java/org/apache/camel/language/xquery/springboot/XQueryLanguageConfiguration.java
+++ components-starter/camel-saxon-starter/src/main/java/org/apache/camel/language/xquery/springboot/XQueryLanguageConfiguration.java
@@ -46,6 +46,11 @@ public class XQueryLanguageConfiguration
      */
     private List<PropertyDefinition> namespace;
     /**
+     * Reference to a org.apache.camel.support.builder.Namespaces bean in the
+     * registry to use for the XML Namespaces of prefix to uri mappings.
+     */
+    private String namespacesRef;
+    /**
      * Source to use, instead of message body. You can prefix with variable:,
      * header:, or property: to specify kind of source. Otherwise, the source is
      * assumed to be a variable. Use empty or null to use default source, which
@@ -72,6 +77,14 @@ public class XQueryLanguageConfiguration
 
     public void setNamespace(List<PropertyDefinition> namespace) {
         this.namespace = namespace;
+    }
+
+    public String getNamespacesRef() {
+        return namespacesRef;
+    }
+
+    public void setNamespacesRef(String namespacesRef) {
+        this.namespacesRef = namespacesRef;
     }
 
     public String getSource() {
--- components-starter/camel-stax-starter/src/main/java/org/apache/camel/language/xtokenizer/springboot/XMLTokenizeLanguageConfiguration.java
+++ components-starter/camel-stax-starter/src/main/java/org/apache/camel/language/xtokenizer/springboot/XMLTokenizeLanguageConfiguration.java
@@ -49,6 +49,11 @@ public class XMLTokenizeLanguageConfiguration
      */
     private List<PropertyDefinition> namespace;
     /**
+     * Reference to a org.apache.camel.support.builder.Namespaces bean in the
+     * registry to use for the XML Namespaces of prefix to uri mappings.
+     */
+    private String namespacesRef;
+    /**
      * Source to use, instead of message body. You can prefix with variable:,
      * header:, or property: to specify kind of source. Otherwise, the source is
      * assumed to be a variable. Use empty or null to use default source, which
@@ -75,6 +80,14 @@ public class XMLTokenizeLanguageConfiguration
 
     public void setNamespace(List<PropertyDefinition> namespace) {
         this.namespace = namespace;
+    }
+
+    public String getNamespacesRef() {
+        return namespacesRef;
+    }
+
+    public void setNamespacesRef(String namespacesRef) {
+        this.namespacesRef = namespacesRef;
     }
 
     public String getSource() {
