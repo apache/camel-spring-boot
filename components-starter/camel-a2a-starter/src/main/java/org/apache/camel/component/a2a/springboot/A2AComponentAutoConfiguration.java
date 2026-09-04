eb7464453b9322fff54583394704bee31ad9218d
oscerd <5106647+oscerd@users.noreply.github.com>
2026-09-04 00:46:41 +0000

    [create-pull-request] automated change

--- components-starter/camel-a2a-starter/src/main/java/org/apache/camel/component/a2a/springboot/A2AComponentAutoConfiguration.java
+++ components-starter/camel-a2a-starter/src/main/java/org/apache/camel/component/a2a/springboot/A2AComponentAutoConfiguration.java
@@ -58,7 +58,8 @@ public class A2AComponentAutoConfiguration {
         return new ComponentCustomizer() {
             @Override
             public void configure(String name, Component target) {
-                CamelPropertiesHelper.copyProperties(target.getCamelContext(), configuration, target);
+                CamelPropertiesHelper.copyConfigurationProperties(target.getCamelContext(), applicationContext,
+                        "camel.component.a2a", configuration, target);
             }
             @Override
             public boolean isEnabled(String name, Component target) {
--- components-starter/camel-alibaba-oss-starter/src/main/java/org/apache/camel/component/alibaba/oss/springboot/OSSComponentAutoConfiguration.java
+++ components-starter/camel-alibaba-oss-starter/src/main/java/org/apache/camel/component/alibaba/oss/springboot/OSSComponentAutoConfiguration.java
@@ -58,7 +58,8 @@ public class OSSComponentAutoConfiguration {
         return new ComponentCustomizer() {
             @Override
             public void configure(String name, Component target) {
-                CamelPropertiesHelper.copyProperties(target.getCamelContext(), configuration, target);
+                CamelPropertiesHelper.copyConfigurationProperties(target.getCamelContext(), applicationContext,
+                        "camel.component.alibaba-oss", configuration, target);
             }
             @Override
             public boolean isEnabled(String name, Component target) {
