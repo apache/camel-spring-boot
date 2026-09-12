1098ebb75e89faa671380f528786fff38781d4ba
Claus Ibsen <claus.ibsen@gmail.com>
2026-09-12 08:29:19 +0200

    Regen

--- components-starter/camel-jgroups-starter/src/main/java/org/apache/camel/component/jgroups/springboot/JGroupsComponentConfiguration.java
+++ components-starter/camel-jgroups-starter/src/main/java/org/apache/camel/component/jgroups/springboot/JGroupsComponentConfiguration.java
@@ -83,6 +83,19 @@ public class JGroupsComponentConfiguration
      * etc.
      */
     private Boolean autowiredEnabled = true;
+    /**
+     * Sets an ObjectInputFilter pattern (jdk.serialFilter syntax) applied as a
+     * defense-in-depth check on the class of the message body deserialized by
+     * org.jgroups.Message.getObject(). The pattern is evaluated after JGroups
+     * has deserialized the payload, so this option alone does not prevent
+     * gadget-chain execution that happens inside the JGroups receive path; to
+     * block such attacks, also configure the JVM-wide -Djdk.serialFilter and
+     * secure the channel with AUTH and encryption. When this option is not set
+     * and no JVM-wide filter is configured, a conservative default filter
+     * denying java.net. and otherwise allowing java., javax. and
+     * org.apache.camel. is applied. Use to accept any type.
+     */
+    private String deserializationFilter;
 
     public JChannel getChannel() {
         return channel;
@@ -130,5 +143,13 @@ public class JGroupsComponentConfiguration
 
     public void setAutowiredEnabled(Boolean autowiredEnabled) {
         this.autowiredEnabled = autowiredEnabled;
+    }
+
+    public String getDeserializationFilter() {
+        return deserializationFilter;
+    }
+
+    public void setDeserializationFilter(String deserializationFilter) {
+        this.deserializationFilter = deserializationFilter;
     }
 }
